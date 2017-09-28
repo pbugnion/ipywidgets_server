a1e387fdb7abd89cc8f96a08604e2f299b101bcf
Pascal Bugnion <pascal@bugnion.org>
2017-09-28 08:09:30 +0100

    Pass module and object as arguments to kernel

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ ioloop.install()  # This needs to happen before the tornado imports
 import os  # noqa: E402
 import shutil  # noqa: E402
 import tempfile  # noqa: E402
+import json
 
 import tornado.ioloop  # noqa: E402
 import tornado.web  # noqa: E402
@@ -13,6 +14,9 @@ import tornado.web  # noqa: E402
 from notebook.services.kernels.handlers import (  # noqa: E402
     MainKernelHandler, ZMQChannelsHandler
 )
+
+from notebook.utils import url_path_join, url_escape
+
 from notebook.services.kernels.kernelmanager \
     import MappingKernelManager  # noqa: E402
 from notebook.services.kernelspecs.handlers \
@@ -20,14 +24,40 @@ from notebook.services.kernelspecs.handlers \
 from jupyter_client.kernelspec \
     import KernelSpecManager  # noqa: E402
 
+from jupyter_client.jsonutil import date_default
+
 
 ROOT = os.path.dirname(__file__)
+CODE = 'test'
+OBJECT = 'vbox'
 
 
 class CustomKernelSpecManager(KernelSpecManager):
 
     def find_kernel_specs(self):
         return {'mod_python': ROOT}
+
+
+class CustomKernelHandler(MainKernelHandler):
+
+    @tornado.web.authenticated
+    @tornado.gen.coroutine
+    def post(self):
+        km = self.kernel_manager
+        model = self.get_json_body()
+        if model is None:
+            model = {
+                'name': km.default_kernel_name
+            }
+        else:
+            model.setdefault('name', km.default_kernel_name)
+
+        kernel_id = yield tornado.gen.maybe_future(km.start_kernel(kernel_name=model['name'], extra_arguments=[CODE, OBJECT]))
+        model = km.kernel_model(kernel_id)
+        location = url_path_join(self.base_url, 'api', 'kernels', url_escape(kernel_id))
+        self.set_header('Location', location)
+        self.set_status(201)
+        self.finish(json.dumps(model, default=date_default))
 
 
 _kernel_id_regex = r"(?P<kernel_id>\w+-\w+-\w+-\w+-\w+)"
@@ -41,7 +71,7 @@ def make_app(connection_dir):
         connection_dir=connection_dir
     )
     handlers = [
-        (r'/api/kernels', MainKernelHandler),
+        (r'/api/kernels', CustomKernelHandler),
         (r'/api/kernels/%s/channels' % _kernel_id_regex, ZMQChannelsHandler),
         (r'/api/kernelspecs', MainKernelSpecHandler),
         (
--- mod_python_kernel.py
+++ mod_python_kernel.py
@@ -1,33 +1,38 @@
+
 from ipykernel.kernelbase import Kernel
 from ipykernel.kernelapp import IPKernelApp
 from ipykernel.ipkernel import IPythonKernel
 
+from traitlets import Unicode
 
-code = """
-from ipywidgets import IntSlider, Text, VBox
-s = IntSlider(max=200, value=100)
-t = Text()
-
-def update_text(change=None):
-    t.value = str(float(s.value) ** 2)
-
-s.observe(update_text, names='value')
-update_text()
-VBox([s, t])
+CODE_TEMPLATE = """
+import {module}
+{module}.{object}
 """
 
 
 class ModPythonKernel(IPythonKernel):
+    exec_module = Unicode()
+    exec_object = Unicode()
 
     msg_types = IPythonKernel.msg_types + ['custom_message']
 
     def custom_message(self, stream, ident, parent):
+        code = CODE_TEMPLATE.format(
+            module=self.user_ns['exec_module'], 
+            object=self.user_ns['exec_object']
+        )
         parent['content'] = {'code': code, 'silent': False}
         super(ModPythonKernel, self).execute_request(stream, ident, parent)
 
     def execute_request(self, stream, ident, parent):
-        raise NotImplementedError()
+        pass
 
 
 if __name__ == '__main__':
-    IPKernelApp.launch_instance(kernel_class=ModPythonKernel)
+    import sys
+    module = sys.argv[3]
+    object = sys.argv[4]
+    IPKernelApp.launch_instance(
+        kernel_class=ModPythonKernel,
+        user_ns=dict(exec_module=module, exec_object=object))
