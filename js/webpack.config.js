d22f65a91b6deb4766704f5a748cfa1b33ba7254
Pascal Bugnion <pascal@bugnion.org>
2017-10-01 07:18:11 +0100

    Clean up webpack config

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -16,19 +16,11 @@ var loaders = [
                             });
                         };
                     }),
-                    postcss.plugin('prepend', function() {
-                        return function(css) {
-                            css.prepend("@import '@jupyter-widgets/controls/css/labvariables.css';")
-                        }
-                    }),
                     require('postcss-import')(),
-                    require('postcss-cssnext')()
                 ]
             }
         }
     ]},
-    // jquery-ui loads some images
-    { test: /\.(jpg|png|gif)$/, use: 'file-loader' },
     // required to load font-awesome
     { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
     { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff' },
