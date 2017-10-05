1aec2038067af60f615ba77c89ac5cf1f57a27f3
Pascal Bugnion <pascal@bugnion.org>
2017-10-05 07:51:43 +0100

    Tidy up loader code

--- js/loader.js
+++ js/loader.js
@@ -3,7 +3,7 @@ var requirePromise = function (pkg) {
     return new Promise(function (resolve, reject) {
         var require = window.require;
         if (require === undefined) {
-            reject("Requirejs is needed, please ensure it is loaded on the page.");
+            reject('Requirejs is needed, please ensure it is loaded on the page.');
         }
         else {
             require(pkg, resolve, reject);
@@ -12,12 +12,11 @@ var requirePromise = function (pkg) {
 };
 
 export function requireLoader(moduleName, moduleVersion) {
-    console.log(`trying to load ${moduleName}`)
     return requirePromise(["" + moduleName]).catch(function (err) {
         var failedId = err.requireModules && err.requireModules[0];
         if (failedId) {
-            console.log("Falling back to unpkg.com for " + moduleName + "@" + moduleVersion);
-            return requirePromise(["https://unpkg.com/" + moduleName + "@" + moduleVersion + "/dist/index.js"]);
+            console.log(`Falling back to unpkg.com for ${moduleName}@${moduleVersion}`);
+            return requirePromise([`https://unpkg.com/${moduleName}@${moduleVersion}/dist/index.js`]);
         }
     });
 }
