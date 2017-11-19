1005e8f921ade835e993a1abf0ea377b23e3dd67
Pascal Bugnion <pascal@bugnion.org>
2017-11-19 07:54:31 +0000

    Remove obsolete default

--- js/output.js
+++ js/output.js
@@ -20,7 +20,6 @@ class OutputModel extends outputBase.OutputModel {
     defaults() {
         return {
             ...super.defaults(),
-            is_capturing: false,
             msg_id: ''
         };
     }
